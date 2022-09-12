2e618ede5b098a4ebe5735e45bb912d38017a692
Simon <simon.montenegro@usach.cl>
2022-09-11 21:04:28 -0300

    Añadidos ejemplos

--- segunda_parte/newtonRaphson.m
+++ segunda_parte/newtonRaphson.m
@@ -1,17 +1,24 @@
 function [solucion] = newtonRaphson(f, iteracion, error, x0)
-    imagen = polyval(f, x0);
+%Se evalua el valor inicial en el polinomio
+    imagen = polyval(f, x0);    
 
+%Se obtiene la derivada del polinomio
     df = derivar(f);
 
+%Se evalua el valor inicial en la derivada del polinomio
     imagen_df = polyval(df, x0);
 
+%Se calcula el nuevo valor (posible solucion)
     x1 = x0 - imagen/imagen_df;
 
+%Se obtiene el error
     nuevo_error = abs(x1 - x0);
 
+%Condiciones de borde
     if(iteracion == 0 || nuevo_error < error)
         solucion = x1;
     else
+        %Recursion
         solucion = newtonRaphson(f, iteracion-1, error, x1);
     end
 
